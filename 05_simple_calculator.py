cbccf6d4383c8e8d63ee5ad20b04f258df8ed684
saifullahhaidari <saifullahhaidari38@gmail.com>
2026-08-03 23:57:30 +0700

    05_simple_calculator.py

---
+++ 05_simple_calculator.py
@@ -0,0 +1,38 @@
+"""
+Project 05: Simple Calculator
+Author: Saifullah Haidari
+
+Description:
+A simple calculator that performs
+basic arithmetic operations.
+"""
+
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+
+print("\n===== Results =====")
+
+print(f"Addition: {number1 + number2}")
+print(f"Subtraction: {number1 - number2}")
+print(f"Multiplication: {number1 * number2}")
+print(f"Division: {number1 / number2}")
+print(f"Floor Division: {number1 // number2}")
+print(f"Remainder: {number1 % number2}")
+print(f"Power: {number1 ** number2}")
+
+نتیجه 
+Enter first number:
+10
+
+Enter second number:
+3
+
+===== Results =====
+
+Addition: 13
+Subtraction: 7
+Multiplication: 30
+Division: 3.3333333333333335
+Floor Division: 3
+Remainder: 1
+Power: 1000
