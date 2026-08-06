eb69b77ba0e98a604af9887d6d37b98a75174777
saifullahhaidari <saifullahhaidari38@gmail.com>
2026-08-06 20:19:31 +0700

    06_comparison_operators.py

---
+++ 06_comparison_operators.py
@@ -0,0 +1,24 @@
+# Comparison Operators in Python
+
+a = 20
+b = 10
+
+print("a =", a)
+print("b =", b)
+
+print("a == b :", a == b)
+print("a != b :", a != b)
+print("a > b  :", a > b)
+print("a < b  :", a < b)
+print("a >= b :", a >= b)
+print("a <= b :", a <= b)
+نتیجه پروژه 
+a = 20
+b = 10
+
+a == b : False
+a != b : True
+a > b  : True
+a < b  : False
+a >= b : True
+a <= b : False
