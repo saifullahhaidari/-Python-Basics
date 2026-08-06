0e54f08873405f94e590573cb560e21e12bebbff
saifullahhaidari <saifullahhaidari38@gmail.com>
2026-08-06 20:21:14 +0700

    07_if_else.py

---
+++ 07_if_else.py
@@ -0,0 +1,15 @@
+# If-Else Statement
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+
+Enter your age: 22
+You are an adult.
+
+  Enter your age: 15
+You are a minor.
